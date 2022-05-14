2cd98cbb4676d113d8a5e58787256a393b61089f
Carbon <robotcreeper13@gmail.com>
2022-05-13 22:50:34 -0500

    Add project files.

---
+++ AutopilotQuick/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutopilotQuick
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public UserDataContext context;
+        public MainWindow()
+        {
+            InitializeComponent();
+            context = new UserDataContext(DialogCoordinator.Instance);
+            DataContext = context;
+        }
+    }
+}
---
+++ AutopilotQuick/UserDataContext.cs
@@ -0,0 +1,39 @@
+﻿using MahApps.Metro.Controls.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutopilotQuick
+{
+    public class UserDataContext : INotifyPropertyChanged
+    {
+        public IDialogCoordinator DialogCoordinator { get; set; }
+        public string Version { get; set; }
+        public string LatestVersion { get; set; }
+
+
+        public UserDataContext(IDialogCoordinator dialogCoordinator)
+        {
+            DialogCoordinator = dialogCoordinator;
+            FileVersionInfo v = FileVersionInfo.GetVersionInfo(App.GetExecutablePath());
+            Version = $"{v.FileMajorPart}.{v.FileMinorPart}.{v.FileBuildPart}";
+        }
+
+
+
+
+
+
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
