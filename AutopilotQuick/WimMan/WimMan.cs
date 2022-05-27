1b5a3c0e1bb9b9adef14c11ae69c7a01ef27e4f8
Carbon <robotcreeper13@gmail.com>
2022-05-27 11:13:57 -0500

    Change wimMan dataURL

--- AutopilotQuick/WimMan/WimMan.cs
+++ AutopilotQuick/WimMan/WimMan.cs
@@ -29,7 +29,7 @@ namespace AutopilotQuick
         public void SetContext(UserDataContext context)
         {
             _context = context;
-            _configCacher = new Cacher("http://sccm2.psd202.org/WIM/WimManConfig.json", "WimManConfig.json", _context);
+            _configCacher = new Cacher("http://nettools.psd202.org/AutoPilotFast/WimManConfig.json", "WimManConfig.json", _context);
             InternetMan.getInstance().InternetBecameAvailable += WimMan_InternetBecameAvailable;
         }
 
