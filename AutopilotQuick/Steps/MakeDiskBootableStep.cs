a728fb98dd8af7c9e215a5178ee598be98c13bfd
Andrew Moore <Amoore@psd202.org>
2022-06-29 06:18:30 -0500

    Remove empty lines from Disk bootable step output.

--- AutopilotQuick/Steps/MakeDiskBootableStep.cs
+++ AutopilotQuick/Steps/MakeDiskBootableStep.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Nito.AsyncEx;
 using NLog;
@@ -19,23 +20,15 @@ namespace AutopilotQuick.Steps
                 
                 IsIndeterminate  = true;
                 var script = @"
-rem == Copy boot files to the System partition ==
 W:\Windows\System32\bcdboot W:\Windows /s S:
-
-:rem == Copy the Windows RE image to the
-:rem    Windows RE Tools partition ==
 md R:\Recovery\WindowsRE
 xcopy /h W:\Windows\System32\Recovery\Winre.wim R:\Recovery\WindowsRE\
-
-:rem == Register the location of the recovery tools ==
 W:\Windows\System32\Reagentc /Setreimage /Path R:\Recovery\WindowsRE /Target W:\Windows
-
-:rem == Verify the configuration status of the images. ==
 W:\Windows\System32\Reagentc /Info /Target W:\Windows
 ";
                 Progress = 50;
                 var output = InvokePowershellScriptAndGetResult(script);
-                Logger.Info($"Bootable output: {output}");
+                Logger.Info($"Output: {Regex.Replace(output, @"^\s*$\n|\r", string.Empty, RegexOptions.Multiline).TrimEnd()}");
                 Progress = 100;
             }
 
