bbd1c1ce5d93443bb444bcf53e1c1e96bde48774
Andrew Moore <robotcreeper13@gmail.com>
2022-08-17 13:55:23 -0500

    Made ApplyDellBiosSettingsStep.cs load files from zip file, instead of resource

--- AutopilotQuick/Resources/DellBiosSettings/BiosEX0.cmd
+++
@@ -1,2 +0,0 @@
-start "bios update" /wait flash64w.exe /b=%1 /s /f /forceit /p=PCSD202
-exit 0
---
+++ AutopilotQuick/Steps/BatchStep.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nito.AsyncEx;
+using NLog;
+
+namespace AutopilotQuick.Steps;
+
+public class BatchStep : StepBaseEx
+{
+    public readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    private List<StepBaseEx> Steps = new List<StepBaseEx>();
+    public BatchStep(List<StepBaseEx> StepsToRun)
+    {
+        Steps = StepsToRun;
+        foreach (var step in Steps)
+        {
+            //Hook the update event for each step
+            step.StepUpdated += StepOnStepUpdated;
+        }
+    }
+    
+    string ComputeSingleStepMessage(StepBase step)
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append(step.Title);
+        if (step.Message != "")
+        {
+            builder.Append(" - ");
+            builder.Append(step.Message);
+        }
+        if (!step.IsIndeterminate)
+        {
+            builder.Append(" - ");
+            builder.Append($"{step.Progress / 100:P0}");
+        }
+
+        return builder.ToString();
+    }
+
+    string ComputeMessageBlock()
+    {
+        var output = new StringBuilder();
+        foreach (var thing in StepTaskMapping)
+        {
+            if (!thing.Value.IsCompleted)
+            {
+                output.AppendLine(ComputeSingleStepMessage(thing.Key));
+            }
+            
+        }
+
+        return output.ToString();
+    }
+    
+    private List<Task<StepResult>> StepTasks = new();
+
+    private Dictionary<StepBase, Task<StepResult>> StepTaskMapping = new Dictionary<StepBase, Task<StepResult>>();
+    public override async Task<StepResult> Run(UserDataContext context, PauseToken pauseToken)
+    {
+        foreach (var step in Steps)
+        {
+            //Add the step run function to the StepTasks list
+            var tas = step.Run(context, pauseToken);
+            
+            StepTasks.Add(tas);
+            StepTaskMapping.Add(step, tas);
+        }
+
+        StepOnStepUpdated(this, new StepStatus());
+        try
+        {
+            await Task.WhenAll(StepTasks);
+        }
+        catch (Exception e)
+        {
+            Logger.Error(e);
+        }
+        
+        return new StepResult(true, $"Completed {Steps.Count} tasks.");
+    }
+    
+    private void StepOnStepUpdated(object? sender, StepStatus e)
+    {
+        Title = $"Running tasks. {StepTasks.Count(x => x.IsCompleted)} of {StepTasks.Count} complete";
+        Message = ComputeMessageBlock();
+        Progress = Steps.Average(x => x.Progress);
+    }
+}
