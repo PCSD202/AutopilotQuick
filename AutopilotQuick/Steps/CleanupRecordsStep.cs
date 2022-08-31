5ed1f06c501b5328014beb1f7e0323d7ea97a4d4
Andrew Moore <robotcreeper13@gmail.com>
2022-08-31 07:35:10 -0500

    Made the CleanupRecordsStep.cs use a configurable URL

--- AutopilotQuick/Steps/CleanupRecordsStep.cs
+++ AutopilotQuick/Steps/CleanupRecordsStep.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,13 +10,15 @@ using Microsoft.ApplicationInsights.Extensibility.Implementation;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Nito.AsyncEx;
+using Polly;
+using Polly.Retry;
 using RestSharp;
 
 namespace AutopilotQuick.Steps;
 
 public class CleanupRecordsStep : StepBaseEx
 {
-    public readonly ILogger Logger = App.GetLogger<FinalizeSyncingLogsStep>();
+    public readonly ILogger Logger = App.GetLogger<CleanupRecordsStep>();
     
     public override string Name() => "Cleanup records step";
     
@@ -34,15 +37,35 @@ public class CleanupRecordsStep : StepBaseEx
         bool IntuneDeviceFound);
 
 
-    public async Task<string> GetAPIKey(UserDataContext context)
+    public class TakeHomeInfo
     {
-        var takehomeCredsCacher = new Cacher("http://nettools.psd202.org/AutoPilotFast/AQTakeHomeAPIKey.txt", "AQTakeHomeAPIKey.txt", context);
+        public string ApiKey { get; set; }
+        public string Url { get; set; }
+    }
+    
+    public async Task<TakeHomeInfo> GetAPIInfo(UserDataContext context)
+    {
+        var takehomeCredsCacher = new Cacher(
+            "https://nettools.psd202.org/AutoPilotFast/Takehome.json",
+            "TakeHome.json", context);
         if (!takehomeCredsCacher.FileCached || !takehomeCredsCacher.IsUpToDate)
         {
             await takehomeCredsCacher.DownloadUpdateAsync();
         }
+
+        try
+        {
+            var info = JsonConvert.DeserializeObject<TakeHomeInfo>(await File.ReadAllTextAsync(takehomeCredsCacher.FilePath));
+            if (info is not null) return info;
+            takehomeCredsCacher.Delete(); //Force it to redownload
+            return await GetAPIInfo(context);
+        }
+        catch (JsonException e)
+        {
+            takehomeCredsCacher.Delete(); //Force it to redownload
+            return await GetAPIInfo(context);
+        }
         
-        return (await File.ReadAllTextAsync(takehomeCredsCacher.FilePath)).Trim();
     }
 
     public override async Task<StepResult> Run(UserDataContext context, PauseToken pauseToken, IOperationHolder<RequestTelemetry> StepOperation)
@@ -70,16 +93,17 @@ public class CleanupRecordsStep : StepBaseEx
         
         Title = "Cleaning up records";
         Progress = 0;
-        IsIndeterminate = true;
+        IsIndeterminate = false;
         
         Message = "Looking up service tag...";
         var serviceTag = GetServiceTag(pauseToken);
-
-        var APIKey = await GetAPIKey(context);
-        Message = "Getting API Key...";
-        var APIURL = $"https://aqtakehome.azurewebsites.net/api/AQDeviceAudit?code={APIKey}";
+        Progress = 25;
         
-        var client = new RestClient(APIURL);
+        Message = "Loading credentials";
+        var APIData = await GetAPIInfo(context);
+        Progress = 50;
+
+        var client = new RestClient(APIData.Url);
         var data = new PostData()
         {
             DeleteAutopilot = context.TakeHomeToggleOn,
@@ -88,19 +112,30 @@ public class CleanupRecordsStep : StepBaseEx
             Dry = false,
             ServiceTagToAudit = serviceTag
         };
+        
         Message = "Executing cleanup...";
         using var t = App.GetTelemetryClient().StartOperation<RequestTelemetry>("Executing delete");
-        t.Telemetry.Url = new Uri(APIURL);
+        t.Telemetry.Url = new Uri(APIData.Url);
         try
         {
-            var response = await client.PostJsonAsync<PostData, ResultStruct>("", data, CancellationToken.None);
-            t.Telemetry.Properties["ResultData"] = JsonConvert.SerializeObject(response, Formatting.Indented);
-            t.Telemetry.Success = true;
-            return new StepResult(true, "Cleaned up records");
+            var request = new RestRequest().AddHeader("x-functions-key", APIData.ApiKey).AddJsonBody(data);
+            await InternetMan.WaitForInternetAsync(context); //Make sure we have internet before we post
+            var response = await client.PostAsync(request);
+            if (response.IsSuccessful)
+            {
+                t.Telemetry.Properties["ResultData"] = JsonConvert.SerializeObject(response, Formatting.Indented);
+                t.Telemetry.Success = true;
+                return new StepResult(true, "Cleaned up records");
+            }
+            t.Telemetry.Success = false;
+            App.GetTelemetryClient().TrackEvent("CleanupError", new Dictionary<string, string>(){{"ServiceTag", GetServiceTag(pauseToken)}});
+            return new StepResult(false, "Failed to process cleanup with azure");
+
         }
         catch
         {
             t.Telemetry.Success = false;
+            App.GetTelemetryClient().TrackEvent("CleanupError", new Dictionary<string, string>(){{"ServiceTag", GetServiceTag(pauseToken)}});
             return new StepResult(false, "Failed to clean up records");
         }
     }
