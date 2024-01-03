fa3b565387e527dad8a3bdb20366c3292b243835
Andrew Moore <robotcreeper13@gmail.com>
2024-01-03 11:31:54 -0600

    Added .Net install step and changed hardware tester location

--- AutopilotQuick/Cacher/CachedResourceURLS.cs
+++ AutopilotQuick/Cacher/CachedResourceURLS.cs
@@ -35,10 +35,11 @@ namespace AutopilotQuick
         public static readonly CachedResourceData OsdImage = new CachedResourceData("OSDCloud_NoPrompt.iso", "OSDImage.iso");
         public static readonly CachedResourceData AzureLogSettings = new CachedResourceData( "AzureLogSettings.json");
         public static readonly CachedResourceData GithubCreds = new CachedResourceData( "GithubCreds.json");
-        public static readonly CachedResourceData HardwareTester = new CachedResourceData( "HardwareTester/HardwareTester.exe", "HardwareTester.exe");
+        public static readonly CachedResourceData HardwareTester = new CachedResourceData( "HardwareTesterFD/HardwareTester.exe", "HardwareTesterFD.exe");
         public static readonly CachedResourceData DellBiosCatalog = new CachedResourceData( "Bios/CatalogPC.json", "CatalogPC.json");
         public static readonly CachedResourceData Flash64W = new CachedResourceData( "Bios/Flash64W.exe", "Flash64W.exe");
         public static readonly CachedResourceData BiosPassword = new CachedResourceData( "Bios/BiosPassword.txt", "BiosPassword.txt");
+        public static readonly CachedResourceData DotnetInstallPackage = new CachedResourceData( "DotnetInstallScript.zip", "DotnetInstallScript.zip");
     }
 }
 
